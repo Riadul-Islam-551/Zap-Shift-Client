ae5a75791ea319fb2887eb573d3d85aa60a1b7f7
Riadul Islam <riadul551@gmail.com>
2025-12-05 18:58:43 +0600

    make the banner section

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WorkingPolicy from "./components/workingPolicy/WorkingPolicy";
 import Service from "./components/service/Service";
+import Banner from "./components/banner/Banner";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <WorkingPolicy></WorkingPolicy>
       <Service></Service>
     </div>
---
+++ src/pages/home/home/components/banner/Banner.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import bannerImg1 from "../../../../../assets/banner/banner1.png";
+import bannerImg2 from "../../../../../assets/banner/banner2.png";
+import bannerImg3 from "../../../../../assets/banner/banner3.png";
+import { FaCircleArrowUp } from "react-icons/fa6";
+
+const Banner = () => {
+  return (
+    <div className="mt-10 mb-16 ">
+      <Carousel autoPlay={true} infiniteLoop={true} className="mx-4">
+        <div className="relative">
+          <img src={bannerImg1} />
+          <div className="absolute z-10 bottom-12 left-16">
+            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+              Track Your Parcel
+              <span className="rotate-45 ">
+                <FaCircleArrowUp className="w-5 h-5 " />
+              </span>
+            </button>
+            <button className="btn  rounded-xl shadow-none border-accent-content">
+              Be A Rider
+            </button>
+          </div>
+        </div>
+        <div className="relative">
+          <img src={bannerImg2} />
+          <div className="absolute z-10 bottom-12 left-16">
+            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+              Track Your Parcel
+              <span className="rotate-45 ">
+                <FaCircleArrowUp className="w-5 h-5 " />
+              </span>
+            </button>
+            <button className="btn  rounded-xl shadow-none border-accent-content">
+              Be A Rider
+            </button>
+          </div>
+        </div>
+        <div className="relative">
+          <img src={bannerImg3} />
+          <div className="absolute z-10 bottom-12 left-16">
+            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+              Track Your Parcel
+              <span className="rotate-45 ">
+                <FaCircleArrowUp className="w-5 h-5 " />
+              </span>
+            </button>
+            <button className="btn  rounded-xl shadow-none border-accent-content">
+              Be A Rider
+            </button>
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
