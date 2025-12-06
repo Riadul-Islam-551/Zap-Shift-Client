616de27cef0ca1b4f3368462dc3ac9b48717a444
Riadul Islam <riadul551@gmail.com>
2025-12-06 09:05:51 +0600

    resize the banner button

--- src/pages/home/home/components/banner/Banner.jsx
+++ src/pages/home/home/components/banner/Banner.jsx
@@ -9,45 +9,45 @@ import { FaCircleArrowUp } from "react-icons/fa6";
 const Banner = () => {
   return (
     <div className="mt-10 mb-16 ">
-      <Carousel autoPlay={true} infiniteLoop={true} className="mx-4">
+      <Carousel autoPlay={true} infiniteLoop={true} className="md:mx-4">
         <div className="relative">
           <img src={bannerImg1} />
-          <div className="absolute z-10 bottom-12 left-16">
-            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+          <div className="absolute z-10 bottom-2 md:bottom-6 lg:bottom-12 left-6 md:left-11 lg:left-16">
+            <button className="btn bg-secondary hover:bg-secondary-content text-white text-xs md:text-sm lg:text-lg rounded-full shadow-none border-none p-6 mr-4 ">
               Track Your Parcel
               <span className="rotate-45 ">
-                <FaCircleArrowUp className="w-5 h-5 " />
+                <FaCircleArrowUp className="w-3 h-3 " />
               </span>
             </button>
-            <button className="btn  rounded-xl shadow-none border-accent-content">
+            <button className="btn rounded-xl shadow-none border-accent-content text-xs md:text-sm lg:text-lg p-6">
               Be A Rider
             </button>
           </div>
         </div>
         <div className="relative">
           <img src={bannerImg2} />
-          <div className="absolute z-10 bottom-12 left-16">
-            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+          <div className="absolute z-10 bottom-2 md:bottom-6 lg:bottom-12 left-6 md:left-11 lg:left-16">
+            <button className="btn bg-secondary hover:bg-secondary-content text-white text-xs md:text-sm lg:text-lg rounded-full shadow-none border-none p-6 mr-4">
               Track Your Parcel
               <span className="rotate-45 ">
                 <FaCircleArrowUp className="w-5 h-5 " />
               </span>
             </button>
-            <button className="btn  rounded-xl shadow-none border-accent-content">
+            <button className="btn rounded-xl shadow-none border-accent-content text-xs md:text-sm lg:text-lg p-6">
               Be A Rider
             </button>
           </div>
         </div>
         <div className="relative">
           <img src={bannerImg3} />
-          <div className="absolute z-10 bottom-12 left-16">
-            <button className="btn bg-secondary text-white rounded-full shadow-none border-none mr-4 ">
+          <div className="absolute z-10 bottom-2 md:bottom-6 lg:bottom-12 left-6 md:left-11 lg:left-16">
+            <button className="btn bg-secondary hover:bg-secondary-content text-white text-xs md:text-sm lg:text-lg rounded-full shadow-none border-none p-6 mr-4 ">
               Track Your Parcel
               <span className="rotate-45 ">
                 <FaCircleArrowUp className="w-5 h-5 " />
               </span>
             </button>
-            <button className="btn  rounded-xl shadow-none border-accent-content">
+            <button className="btn rounded-xl shadow-none border-accent-content text-xs md:text-sm lg:text-lg p-6">
               Be A Rider
             </button>
           </div>
